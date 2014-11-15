c2edf3a4e4bfc530321d74c02410c793bb73d735
Minghao Xu <xianyuhuanyue@gmail.com>
2014-11-15 21:37:21 +0800

    完成了最后的手势添加识别，同时做了限定

--- src/com/eva/me/hackshanghai_diffchoice/Camera2FileActivity.java
+++ src/com/eva/me/hackshanghai_diffchoice/Camera2FileActivity.java
@@ -3,6 +3,9 @@ package com.eva.me.hackshanghai_diffchoice;
 import java.io.File;
 import java.io.IOException;
 
+import com.eva.me.hackshanghai_diffchoice.listener.ZoomListener;
+import com.eva.me.hackshanghai_diffchoice.listener.onZoomLargeListener;
+import com.eva.me.hackshanghai_diffchoice.listener.onZoomSmallListener;
 import com.eva.me.hackshanghai_diffchoice.util.FileUtils;
 
 import android.app.Activity;
@@ -18,12 +21,23 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Camera2FileActivity extends Activity {
 
 	private Button btnCamera;
+	private Bitmap bm;
 	private ImageView ivReveal;
+	private TextView tvRight, tvTop;
 	private String TAG = "Camera2FileActivity";
+	
+	private int sizeVertical = 2;
+	private int sizeHorizontal = 2;
+	//max value 
+	private final int maxHorizontalSize  = 20;
+	private final int maxVerticalSize = 20;
+	
+	private boolean hasResult  = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +50,8 @@ public class Camera2FileActivity extends Activity {
 		//init views
 		btnCamera = (Button) findViewById( R.id.button1);
 		ivReveal = (ImageView) findViewById( R.id.imageView1);
-		
-		Log.e(TAG, "jin rule ");
+		tvRight = (TextView) findViewById(R.id.tvRight);
+		tvTop = (TextView) findViewById(R.id.tvTop);
 		
 		btnCamera.setOnClickListener(new OnClickListener() {
 			
@@ -53,6 +67,50 @@ public class Camera2FileActivity extends Activity {
 			
 		});
 		
+		ZoomListener mZoomListener = new ZoomListener();
+		mZoomListener.setOnZoomLargeListener(new onZoomLargeListener() {
+			
+			@Override
+			public void onZoomLarge(int direction) {
+				switch (direction) {
+				case ZoomListener.LeftRight:
+//					sizeHorizontal+=1;
+					sizeHorizontal = (sizeHorizontal+1)>maxHorizontalSize? maxHorizontalSize: (sizeHorizontal+1) ;
+					tvTop.setText("当前横向格子数："+sizeHorizontal);
+					break;
+				case ZoomListener.UpDown:
+//					sizeVertical+=1;
+					sizeVertical = (sizeVertical+1) > maxVerticalSize?maxVerticalSize:(sizeVertical+1);
+					tvRight.setText("当前纵向格子数："+sizeVertical);
+					break;
+
+				default:
+					break;
+				}
+			}
+		});
+		mZoomListener.setOnZoomSmallListener(new  onZoomSmallListener() {
+			
+			@Override
+			public void onZoomSmall(int direction) {
+				switch (direction) {
+				case ZoomListener.LeftRight:
+//					sizeHorizontal-=1;
+					sizeHorizontal = (sizeHorizontal-1)<2?2:(sizeHorizontal-1);
+					tvTop.setText("当前横向格子数："+sizeHorizontal);  
+					break;
+				case ZoomListener.UpDown:
+//					sizeVertical-=1;
+					sizeVertical = (sizeVertical-1)<2?2:(sizeVertical-1);
+					tvRight.setText("当前纵向格子数："+sizeVertical);
+					break;
+
+				default:
+					break;
+				}
+			}
+		});
+		ivReveal.setOnTouchListener(mZoomListener);
 	}
 	
 	@Override
@@ -63,7 +121,7 @@ public class Camera2FileActivity extends Activity {
 			Log.d(TAG, "onActivityResult in");//曲线救国喽~
 			Bitmap bm = FileUtils.getLoacalBitmap();
 			ivReveal.setImageBitmap(bm);
-			
+			hasResult = true;
 		}
 		
 		super.onActivityResult(requestCode, resultCode, data);
